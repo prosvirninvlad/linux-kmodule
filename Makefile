190d71d8a6c918be8803f2b0f005b3ca592dba9d
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-04 01:04:05 +0300

    Решение задачи "Fortune".

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 obj-m += kmodule.o
 
+BUILD = make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd)
+
 all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) modules
+	$(BUILD) modules
 
 clean:
-	make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) clean
-
-clear:
-	rm -rf .*.*o* *.*o* *.symvers *.tmp*
+	$(BUILD) clean
--- kmodule.c
+++ kmodule.c
@@ -1,20 +1,63 @@
+#include <asm/uaccess.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/vmalloc.h>
 
-int kmodule_proc_open(struct inode *inode, struct file *file) {
+/* Буфер для хранения "высказываний" */
+static const int mesg_max_len = PAGE_SIZE;
+static int mesg_cur_len = 0;
+static int mesg_next = 0;
+static int len_check = 1;
+static char *mesg_buf;
+
+static const char *kmodule_proc_en = "kmodule";
+
+static int kmodule_proc_open(struct inode *inode, struct file *file) {
 	return 0;
 }
 
-int kmodule_proc_release(struct inode *inode, struct file *file) {
+static int kmodule_proc_release(struct inode *inode, struct file *file) {
 	return 0;
 }
 
-ssize_t kmodule_proc_read(struct file *file, char __user *buf, size_t size, loff_t *offset) {
-	return 0;
+static ssize_t kmodule_proc_read(struct file *file, char __user *buf, size_t size, loff_t *offset) {
+	
+	int len = 0;
+
+	/* Костыль от бесконечного цикла */
+	if (len_check) {
+		len_check = 0;
+	} else {
+		len_check = 1;
+		return 0;
+	}
+
+	if (mesg_next >= mesg_cur_len) {
+		mesg_next = 0;
+	}
+
+	len = sprintf(buf, "%s\n", &mesg_buf[mesg_next]);
+	mesg_next += len;
+
+	return len;
+
 }
 
-ssize_t kmodule_proc_write(struct file *file, const char __user *buf, size_t size, loff_t *offset) {
+static ssize_t kmodule_proc_write(struct file *file, const char __user *buf, size_t size, loff_t *offset) {
+
+	if (size > (mesg_max_len - mesg_cur_len + 1)) {
+		return -ENOMEM;
+	}
+
+	if (copy_from_user(&mesg_buf[mesg_cur_len], buf, size)) {
+		return -EFAULT;
+	}
+
+	mesg_cur_len += size;
+	mesg_buf[mesg_cur_len - 1] = 0;
+
 	return size;
+
 }
 
 static struct file_operations fops = {
@@ -25,12 +68,26 @@ static struct file_operations fops = {
 };
 
 static int __init kmodule_init(void) {
-	proc_create("kmodule", 0644, NULL, &fops);
+
+	proc_create(kmodule_proc_en, 0644, NULL, &fops);
+
+	mesg_buf = (char*) vmalloc(mesg_max_len);
+
+	if (!mesg_buf) {
+		remove_proc_entry(kmodule_proc_en, NULL);
+		return -ENOMEM;
+	}
+
+	memset(mesg_buf, 0, mesg_max_len);
+	mesg_cur_len = mesg_next = 0;
+
 	return 0;
+
 }
 
 static void __exit kmodule_exit(void) {
-	remove_proc_entry("kmodule", NULL);
+	remove_proc_entry(kmodule_proc_en, NULL);
+	vfree(mesg_buf);
 }
 
 MODULE_LICENSE("GPL");
