f0d4136356c6763d8119e1caef405e1db0ffac75
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-04 00:06:21 +0300

    Внедрение возможности создания файла в proc.

--- kmodule.c
+++ kmodule.c
@@ -1,10 +1,15 @@
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
-static int kmodule_init(void) {
+static struct file_operations fops;
+
+static int __init kmodule_init(void) {
+	proc_create("kmodule", 0644, NULL, &fops);
 	return 0;
 }
 
-static void kmodule_exit(void) {
+static void __exit kmodule_exit(void) {
+	remove_proc_entry("kmodule", NULL);
 }
 
 MODULE_LICENSE("GPL");
