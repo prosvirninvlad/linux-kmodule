343579dd7009876d524504eb87ea95f3b614857d
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-04 00:15:06 +0300

    Определение callback'ов для proc.

--- kmodule.c
+++ kmodule.c
@@ -1,7 +1,28 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
-static struct file_operations fops;
+int kmodule_proc_open(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+int kmodule_proc_release(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+ssize_t kmodule_proc_read(struct file *file, char __user *buf, size_t size, loff_t *offset) {
+	return 0;
+}
+
+ssize_t kmodule_proc_write(struct file *file, const char __user *buf, size_t size, loff_t *offset) {
+	return size;
+}
+
+static struct file_operations fops = {
+	.open = kmodule_proc_open,
+	.read = kmodule_proc_read,
+	.write = kmodule_proc_write,
+	.release = kmodule_proc_release
+};
 
 static int __init kmodule_init(void) {
 	proc_create("kmodule", 0644, NULL, &fops);
